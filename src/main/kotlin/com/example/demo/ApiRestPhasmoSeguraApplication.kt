e1480b77c80fd9ad2a37d39b2c9fb7786fae57f7
aaracan246 <acalberto@iesrafaelalberti.es>
2024-12-10 11:36:32 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/demo/ApiRestPhasmoSeguraApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiRestPhasmoSeguraApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiRestPhasmoSeguraApplication>(*args)
+}
